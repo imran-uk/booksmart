f1c51ab1ab247777986583a742fdcfe70247eed5
Imran Chaudhry <imran.chaudhry@jobsite.co.uk>
2019-08-09 11:48:26 +0100

    add Book class

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace booksmart.Models
+{
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+
+}
